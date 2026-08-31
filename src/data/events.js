4dd37bec186dad5b33b3b7308c394c751ae50bfb
Joe McCall <joemccall86@gmail.com>
2026-08-31 16:32:32 +0000

    Add Miles N Mugs 80s Style event (Thu Sep 10)

--- src/data/events.js
+++ src/data/events.js
@@ -1,5 +1,14 @@
 export const events = [
   {
+    title: 'Miles N Mugs 80\'s Style',
+    date: 'Thu, Sep 10, 2026',
+    time: '6:30 PM',
+    location: 'Celery City Craft',
+    address: 'Downtown Sanford, FL',
+    url: 'https://www.facebook.com/events/s/miles-n-mugs-80s-style/1293019652788998/',
+    type: 'pink'
+  },
+  {
     title: 'Sunday Funday Run and Potluck',
     date: 'Sun, Sep 13, 2026',
     time: '7:30 AM',
