40b6a4e4c2b8b209b3899029f65e165d52444fc1
Joe McCall <joemccall86@gmail.com>
2026-09-13 23:29:15 +0000

    Add 80's album to photo gallery with Sep 13 event photos

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -24,6 +24,13 @@ import thu18 from '../assets/Thursday/img_a9ee0d07ea41.jpeg';
 import thu19 from '../assets/Thursday/img_2ff2fa0d57e3.jpeg';
 import thu20 from '../assets/Thursday/img_dfc0d9997df0.jpeg';
 
+import eighty0 from '../assets/80s/img_f8bb540b4e09.jpeg';
+import eighty1 from '../assets/80s/img_b395adf5efc7.jpeg';
+import eighty2 from '../assets/80s/img_288e5c8ed778.jpeg';
+import eighty3 from '../assets/80s/img_a9ee0d07ea41.jpeg';
+import eighty4 from '../assets/80s/img_2ff2fa0d57e3.jpeg';
+import eighty5 from '../assets/80s/img_dfc0d9997df0.jpeg';
+
 import sun0 from '../assets/Sunday/mullet-foot-1.jpg';
 import sun1 from '../assets/Sunday/mullet-foot-2.jpg';
 import sun2 from '../assets/Sunday/mullet-foot-3.jpg';
@@ -51,6 +58,7 @@ import sun22 from '../assets/Sunday/sunday-sunset-horizon.jpg';
 const albums = {
   Thursday: [thu0, thu1, thu2, thu3, thu4, thu5, thu6, thu7, thu8, thu9, thu10, thu11, thu12, thu13, thu14, thu15, thu16, thu17, thu18, thu19, thu20].map((img) => ({ original: img, thumbnail: img })),
   Sunday: [sun0, sun1, sun2, sun3, sun4, sun5, sun6, sun7, sun8, sun9, sun10, sun11, sun12, sun13, sun14, sun15, sun16, sun17, sun18, sun19, sun20, sun21, sun22].map((img) => ({ original: img, thumbnail: img })),
+  "80's": [eighty0, eighty1, eighty2, eighty3, eighty4, eighty5].map((img) => ({ original: img, thumbnail: img })),
 };
 
 const Photos = () => {
